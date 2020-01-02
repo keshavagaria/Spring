7420c216fd5811fb4867f395541b5e26b0f91efd
vishal.singh <vishal.singh@A1-AI-D01.mpllc.com>
2020-01-03 03:53:45 +0530

    springmvc-security using annotation configuration

---
+++ SpringMVC-Security-Annotations/src/main/java/com/mobileprogramming/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.mobileprogramming;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
